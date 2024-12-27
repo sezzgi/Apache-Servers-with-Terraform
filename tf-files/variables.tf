7ad101a06c7dc5c38caf993b16123ce66b86e77b
sez <sezozrn@gmail.com>
2024-12-26 22:01:34 -0800

    changed files

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "sezzgi"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "firstkey"
+}
+
+variable "hosted-zone" {
+  default = "clarusway.us"
+}
